518cef04901f0ca0247f90feaaea38dd29886c48
namantia1@gmail.com <namantia1@gmail.com>
2020-11-07 17:25:58 -0500

    user can register frontend

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,19 +1,27 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
-const Auth = () => {
+const Auth = (props) => {
 
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
 
-    
+    const register = () => {
+        axios.post('/api/auth/register', {username, password})
+        
+    }
 
-
+      
     return(
         <div>
-           <input type="text" name="username" placeholder="username"/>
-           <input type="text" name="password" placeholder="pasword"/>
+           <input type="text" name="username" placeholder="username" onChange={(e) => setUsername(e.target.value)}/>
+           <input type="text" name="password" placeholder="pasword" onChange={(e) => setPassword(e.target.value)}/>
+        
+           <button onClick={() => register()}>Register</button>
+        
         </div>
+
+        
     )
 }
 
