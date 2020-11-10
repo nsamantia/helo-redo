6fcc2c3d7e34389863d0e80e619e484d83bce442
namantia1@gmail.com <namantia1@gmail.com>
2020-11-10 17:47:43 -0500

    can get posts frontend

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,12 +1,23 @@
 import axios from 'axios'
 import React, {useState, useEffect} from 'react'
+import {connect} from 'react-redux'
 import './Dashboard.css'
+import DashboardMap from './DashboardMap'
 
-const Dashboard = () => {
 
-const [posts, setPosts] = useState([])
-const [search, setSearch] = useState('')
-const [userPosts, setUserPosts] = useState(false)
+const Dashboard = (props) => {
+
+    const [posts, setPosts] = useState([])
+    const [search, setSearch] = useState('')
+    const [userPosts, setUserPosts] = useState(false)
+
+    useEffect(() => {
+        axios.get(`/api/posts/${props.user.id}?search=${search}&userPosts=${userPosts}`)
+        .then(res => setPosts(res.data))
+    }, [])
+
+
+
 
     return(
         <div className="dashboard-container">
@@ -17,8 +28,23 @@ const [userPosts, setUserPosts] = useState(false)
                 <button>Search</button>
                 <button>Reset</button>
             </div>
+
+            {posts.map(e => {
+                return(
+                    < DashboardMap
+                        post={e}
+                        key={e.id}
+                        title={e.title}
+                        username={e.username}
+                        
+                        
+                    />
+                )
+            })}
         </div>
     )
 }
 
-export default Dashboard
+const mapStateToProps = reduxState => reduxState
+
+export default connect(mapStateToProps)(Dashboard)
---
+++ src/components/Dashboard/DashboardMap.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const DashboardMap = (props) => {
+
+    return(
+        <div>
+           <p>hello from dashboardmap</p>
+            <p>{props.title}</p>
+            <p>{props.username}</p>
+            
+
+        </div>
+    )
+}
+
+export default DashboardMap
