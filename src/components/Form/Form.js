ad09ef3bb4e1c22e1ea30f478275367bf45131e5
namantia1@gmail.com <namantia1@gmail.com>
2020-11-12 16:19:38 -0500

    user can post a new post

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,12 +1,16 @@
 import React, {useState} from 'react'
+import axios from 'axios'
 import {connect} from 'react-redux'
 
-const Form = () =>{
+const Form = (props) =>{
 
     const [title, setTitle] = useState('')
     const [image, setImage] = useState('')
     const [content, setContent] = useState('')
 
+    const newPost = () => {
+        axios.post(`/api/post/${props.user.id}`,{title, image, content})
+    }
 
     return(
         <div>
@@ -14,6 +18,7 @@ const Form = () =>{
             <input type='text' name ='title' placeholder='title' onChange={(e)=> setTitle(e.target.value)}/>
             <input type='text' name ='image' placeholder='image'onChange={(e)=> setImage(e.target.value)}/>
             <input type='text' name ='content' placeholder='content' onChange={(e)=>setContent(e.target.value)}/>
+            <button onClick ={() => newPost()}>Post</button>
         </div>
     )
 }
